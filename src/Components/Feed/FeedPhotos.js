5a588354c34a8e773e282e615af9f2ecef700e80
Ricardo Gusi <ricardogusi@hotmail.com>
2020-08-21 10:47:15 -0300

    construção do modal dentro do feed

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FeedPhotosItem from './FeedPhotosItem';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTOS_GET } from '../../Api';
@@ -6,10 +6,10 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
   const { data, loading, error, request } = useFetch();
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { json } = await request(url, options);
@@ -24,7 +24,7 @@ const FeedPhotos = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotosItem key={photo.id} photo={photo} />
+          <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto} />
         ))}
       </ul>
     );
