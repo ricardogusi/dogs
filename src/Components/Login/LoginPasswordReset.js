2639b54ec7dbef135beb7874b230a052d98341a9
Ricardo Gusi <ricardogusi@hotmail.com>
2020-08-25 09:54:20 -0300

    ajustes finais

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -37,7 +37,7 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Resete sua Senha" />
 
       <h1 className="title">Resete a Senha</h1>
@@ -50,7 +50,7 @@ const LoginPasswordReset = () => {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
